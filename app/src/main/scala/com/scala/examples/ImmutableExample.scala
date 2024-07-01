8fbf49d983d1e87c974519a6fe6931bb66ea987e
Neha Sardana <neha.sardana97@gmail.com>
2024-07-01 16:32:20 -0400

    Updates to ImmutableExample

--- app/src/main/scala/com/scala/examples/ImmutableExample.scala
+++ app/src/main/scala/com/scala/examples/ImmutableExample.scala
@@ -1,12 +1,14 @@
 package com.scala.examples
 
 object ImmutableExample extends App{
-  
+
   val youCannotChangeMe = 5
   var changeMe = 10
   print(s"The value of variable youCannotChangeMe: $youCannotChangeMe" )
   print(s"The value of variable changeMe: $changeMe")
  //youCannotChangeMe = youCannotChangeMe+1 //compiler error
-  
+
+  val numbers = List(1, 2, 3, 4, 5) // Immutable list
+  val doubled = numbers.map(_ * 2)
 
 }
